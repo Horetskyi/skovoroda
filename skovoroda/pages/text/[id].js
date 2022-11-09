8683b8f2276c1d61890947555a56900fdc8dd2b6
Horetskyi <horetskiy.d.s.7@gmail.com>
2022-11-09 21:11:52 +0200

    redundant code removed

--- skovoroda/pages/text/[id].js
+++ skovoroda/pages/text/[id].js
@@ -21,7 +21,6 @@ export default function Text({ textData }) {
       translatorName={translation.translator.fullName}
       translatedName={translation.translatedName}
       downloadFileName="Г. С. Сковорода - Повна академічна збірка - Леонід Ушкалов - 2011.pdf"
-      downloadUrl="https://skovoroda.s3.eu-west-3.amazonaws.com/sources/%D0%93.+%D0%A1.+%D0%A1%D0%BA%D0%BE%D0%B2%D0%BE%D1%80%D0%BE%D0%B4%D0%B0+-+%D0%9F%D0%BE%D0%B2%D0%BD%D0%B0+%D0%B0%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D1%96%D1%87%D0%BD%D0%B0+%D0%B7%D0%B1%D1%96%D1%80%D0%BA%D0%B0+-+%D0%9B%D0%B5%D0%BE%D0%BD%D1%96%D0%B4+%D0%A3%D1%88%D0%BA%D0%B0%D0%BB%D0%BE%D0%B2+-+2011.pdf"
     />
   }); 
 
